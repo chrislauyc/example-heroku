1cf8021a6059ca74f520519fa654756aaa55a2f0
Chris Lau <chrislyc.lau@gmail.com>
2021-08-11 21:57:21 -0600

    fixed path error

--- api/server.js
+++ api/server.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 const server = express();
 server.use(express.json());
 server.use(cors());
-// when you go to the home page, take you to the frontend
-server.use(express.static(path.join(__dirname,"client/build")));
+
 
 server.use("/api",(req,res)=>res.status(200).json({data:"endpoint is working"}));
-server.use("*",(req,res)=>{
-    //handles any unmatched routes
-    res.sendFile(path.join(__dirname,"client/build","index.html"));
-})
+
 
 
 module.exports=server;
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 require("dotenv").config();
 const server = require("./api/server");
+const express = require("express");
+const path = require("path");
+
 const port = process.env.PORT || "1234"
-console.log(__dirname,__filename);
+// console.log(__dirname,__filename);
+// when you go to the home page, take you to the frontend
+server.use(express.static(path.join(__dirname,"client/build")));
+server.use("*",(req,res)=>{
+    //handles any unmatched routes
+    res.sendFile(path.join(path.normalize(`${__dirname}/..`),"client/build","index.html"));
+})
 server.listen(port,()=>console.log("server is listening"));
