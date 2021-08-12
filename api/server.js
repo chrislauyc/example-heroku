573580656451a2ffa0567b5550b5a2f68165c9e6
Chris Lau <chrislyc.lau@gmail.com>
2021-08-11 19:36:39 -0600

    fixed home path

--- api/server.js
+++ api/server.js
@@ -7,7 +7,6 @@ server.use(cors());
 // when you go to the home page, take you to the frontend
 server.use(express.static(path.join(__dirname,"client/build")));
 
-server.use("/",(req,res)=>res.status(200).json({message:"endpoint is working"}));
 server.use("/api",(req,res)=>res.status(200).json({data:"endpoint is working"}));
 server.use("*",(req,res)=>{
     //handles any unmatched routes
